d2db8d3d6f946a27301dcdfc6ccfc1739f3732db
Jayesh Bairagi <jayesh@ost.com>
2020-02-25 15:13:46 +0530

    Updated site config

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'MosaicDAO', // Title for your website.
   tagline: 'A website for Mosaic',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  url: 'https://jayeshbairagi.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'mosaicdao-website',
+  projectName: 'mosaicdaoWebsite',
   organizationName: 'Mosaic',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
